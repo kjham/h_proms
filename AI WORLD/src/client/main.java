4270fe145368da981b84af8bf90932bb6bb55d19
proms <h_proms@naver.com>
2017-10-30 19:39:20 +0900

    신규 클래스 추가
    Main Class 생성

---
+++ AI WORLD/src/client/main.java
@@ -0,0 +1,15 @@
+package client;
+
+/**
+ * @date 2017. 10. 30
+ * @author KJ 
+ * @descroption Client 측 메인 클래스
+ */
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to AI WORLD !");
+	}
+
+}
